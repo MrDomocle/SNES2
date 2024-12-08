8695dfa4a1ca1786b372fddb984d887542468a38
Alex G <frankfortdomocle@gmail.com>
2024-12-08 19:47:06 +0700

    gfx loader for 2bpp

---
+++ README.md
@@ -0,0 +1,3 @@
+# Space shooter game for SNES
+This is my first major project for the SNES, written in assembly using cc65.  
+It's written from scratch - I didn't use libraries that add a lot of functionality that I would otherwise code for myself.
--- main.asm
+++ main.asm
@@ -1,8 +1,14 @@
 .p816
-.i16
-.a8
+.smart
 
 .include "snes.inc"
+.include "macros.inc"
+.include "charset.asm"
+
+VRAM_CHARS = $0000
+VRAM_BG1 = $0000
+LOAD_INDEX_1 = $00
+LOAD_INDEX_2 = $02
 
 .segment "CODE"
 .proc ResetHandler
@@ -10,6 +16,51 @@
    sei ; disable interrupts
    clc ; carry clear
    xce ; swap carry and emulation bits
+   
+   setXY16 ; X needs to be 16-bit to set the vram add address
+   ; load character data to VRAM
+   lda #$80
+   sta VMAIN ; set vram increment mode
+   ldx #VRAM_CHARS
+   stx VMADDL ; set vram load start address
+   setXY8 ; dont need that anymore
+
+   ; loop
+   stz LOAD_INDEX_1 ; bp1
+   lda #8
+   sta LOAD_INDEX_2 ; bp2
+
+   ldy #0
+   @loop:
+      ldx LOAD_INDEX_1
+      lda charstart,x
+      sta VMDATAL
+      inx
+      stx LOAD_INDEX_1
+      
+      ldx LOAD_INDEX_2
+      lda charstart,x
+      sta VMDATAH ; write to high increments vram address
+      inx
+      stx LOAD_INDEX_2
+ 
+      iny
+      cpy #8 ; check if a character was loaded completely
+      bne @nojump
+      
+      ldy #0
+      lda LOAD_INDEX_1
+      adc #7
+      sta LOAD_INDEX_1
+
+      lda LOAD_INDEX_2
+      adc #8
+      sta LOAD_INDEX_2
+ 
+      @nojump:
+ 
+      cpx #(charend - charstart) ; compare x to number of bytes for chars
+      bne @loop
 
    ; enable non-maskable interrupt
    lda #$81
