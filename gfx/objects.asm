52d0623739fd53d50968e40778b6c95cfd4bebac
Alex G <frankfortdomocle@gmail.com>
2024-12-11 13:24:09 +0700

    make bullet pool a .repeat block

--- gfx/objects.asm
+++ gfx/objects.asm
@@ -11,16 +11,15 @@ enemy = 4*1
 .byte %00100000 ; yyyyyyyy
 .byte %00000010 ; tttttttt
 .byte %00010000 ; vhppccct
-bullet1 = 4*2
+; bullet pool
+bullet_first = 4*2
+.repeat 10
 .byte %00100000 ; xxxxxxxx
 .byte %11110111 ; yyyyyyyy
 .byte %00000100 ; tttttttt
 .byte %00000000 ; vhppccct
-bullet2 = 4*3
-.byte %00100000 ; xxxxxxxx
-.byte %11110111 ; yyyyyyyy
-.byte %00000100 ; tttttttt
-.byte %00000000 ; vhppccct
+.endrepeat
+bullet_last = 4*13
 objloend:
 
 objhistart:
