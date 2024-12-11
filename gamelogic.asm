e7e42c92e7173c52e4a1ae1d8e9f074c1195f345
Alex G <frankfortdomocle@gmail.com>
2024-12-11 13:24:30 +0700

    make movement of ship bounded

--- gamelogic.asm
+++ gamelogic.asm
@@ -1,5 +1,9 @@
 SHOT_INTERVAL = 6 ; frames of cooldown between shots
 BULLET_SPEED = 3 ; speed of bullets in pixels per frame
+SHIP_BOUND_Y_HI = $cf
+SHIP_BOUND_Y_LO = $20
+SHIP_BOUND_X_HI = $f0
+SHIP_BOUND_X_LO = $00
 .segment "CODE"
 .proc UpdateCooldowns
    setAXY16
@@ -20,27 +24,23 @@ BULLET_SPEED = 3 ; speed of bullets in pixels per frame
    lda joy1_buffer
    bit #$0200 ; left
    beq @not_l
-      setA8
-      dec oam_lo+xc+ship
-      setA16
+      ldx #%10
+      jsr MoveShip
    @not_l:
    bit #$0100 ; right
    beq @not_r
-      setA8
-      inc oam_lo+xc+ship
-      setA16
+      ldx #%11
+      jsr MoveShip
    @not_r:
    bit #$0800 ; up
    beq @not_u
-      setA8
-      dec oam_lo+yc+ship
-      setA16
+      ldx #%00
+      jsr MoveShip
    @not_u:
    bit #$0400 ; down
    beq @not_d
-      setA8
-      inc oam_lo+yc+ship
-      setA16
+      ldx #%01
+      jsr MoveShip
    @not_d:
 
    bit #$8000 ; b
@@ -60,20 +60,22 @@ BULLET_SPEED = 3 ; speed of bullets in pixels per frame
    bne @return ; return if nmi_count is less
       ldx #SHOT_INTERVAL
       stx shot_cooldown ; reset cooldown
-
-   lda oam_lo+yc+bullet1
-   cmp #$f7
-   beq @b1_free
-      ; if 1st bullet object isn't free (not hidden), check 2nd
-      lda oam_lo+yc+bullet2
+   
+   ldx #bullet_first
+   @loop:
+      lda oam_lo+yc,x
       cmp #$f7
-      bne @return ; ignore call if no bullets free
-         ; use bullet2 otherwise
-         ldx #bullet2
-         bra @apply
-   @b1_free:
-      ldx #bullet1
+      bne @continue ; skip this bullet if it's not hidden
+      ; otherwise use that bullet
       bra @apply
+
+      @continue:
+      .repeat 4
+         inx
+      .endrepeat
+      cpx #bullet_last
+      bne @loop
+   
    @apply:
       lda oam_lo+xc+ship
       adc #3
@@ -86,38 +88,66 @@ BULLET_SPEED = 3 ; speed of bullets in pixels per frame
    setA16
    rts
 .endproc
+.proc MoveShip ; load direction to X before calling. bits 0,1: 00 up 01 down 10 left 11 right
+   setA8
+   cpx #%00 ; up
+   bne @not_up
+      lda oam_lo+yc+ship
+      cmp #SHIP_BOUND_Y_LO
+      beq @not_up ; skip if at lower (top of screen) bound
+         ; otherwise move up
+         dec oam_lo+yc+ship
+   @not_up:
+   cpx #%01 ; down
+   bne @not_down
+      lda oam_lo+yc+ship
+      cmp #SHIP_BOUND_Y_HI
+      beq @not_down
+         inc oam_lo+yc+ship
+   @not_down:
+   cpx #%10 ; left
+   bne @not_left
+      lda oam_lo+xc+ship
+      cmp #SHIP_BOUND_X_LO
+      beq @not_left
+         dec oam_lo+xc+ship
+   @not_left:
+   cpx #%11 ; right
+   bne @not_right
+      lda oam_lo+xc+ship
+      cmp #SHIP_BOUND_X_HI
+      beq @not_right
+         inc oam_lo+xc+ship
+   @not_right:
+   @return:
+   setA16
+   rts
+.endproc
 .proc TickBullets
    setA8
-   lda oam_lo+yc+bullet1
-   cmp #$f6 ; check if 1st bullet is active (if yc is more than f6 it's hidden)
-   bcs @b1_out
-      ; move bullet1
-      lda oam_lo+yc+bullet1
+   ldx #bullet_first
+   @loop: ; iterate through every bullet and tick it if it's on screen
+      lda oam_lo+yc,x
+      cmp #$f6
+      bcs @continue ; skip this bullet if it's offscreen (register > #$f6)
+
       sbc #BULLET_SPEED
-      bcc @b1_over ; return if y-coordinate overflew (end of screen)
-      sta oam_lo+yc+bullet1
-   @b1_out:
-   lda oam_lo+yc+bullet2
-   cmp #$f6
-   bcs @b2_out
-      lda oam_lo+yc+bullet2
-      sbc #BULLET_SPEED
-      bcc @b2_over
-      sta oam_lo+yc+bullet2
-   @b2_out:
-      bra @return
-   @b1_over:
-      ldx #bullet1
-      jsr HideObject
-      bra @return
-   @b2_over:
-      ldx #bullet2
-      jsr HideObject
-      bra @return
+      bcs @not_over
+         jsr HideObject ; hide if yc overflows
+         bra @continue
+      @not_over:
+      sta oam_lo+yc,x
+      
+      @continue:
+      .repeat 4
+         inx
+      .endrepeat
+      cpx #bullet_last
+      bne @loop
    @return:
    rts
 .endproc
-.proc HideObject ; put obj offscreen. obj's offset has to be in x
+.proc HideObject ; put obj offscreen. obj's offset in oam_lo has to be in X before call
    lda #$f7
    sta oam_lo+yc,x
    rts
